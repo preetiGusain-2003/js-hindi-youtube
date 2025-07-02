29bdf9c984c6f06297439ae81e4a9210f9de9196
preetiGusain-2003 <gusainpreeti60@gmail.com>
2025-07-02 18:18:19 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+var userNmae = "preeti";
+let userSurname="gusain";
+const userGender="female";
+userAge="22";//we can also define variables like this but it is not good practise .
+var userAddress;//we can also declare varibles without defining but it give undefined as output .
+console.table([userNmae,userSurname,userGender,userAge]);
+/*prefer not to use var because of issue in block and functional scope*/
